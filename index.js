a36b6bcb462cd4e9b662e957179e7f71684c2335
ToAnh Tran <toanh.t.tran@gmail.com>
2018-10-19 17:19:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ function distanceFromHqInBlocks(blockNumber) {
 function distanceFromHqInFeet(blockNumber) {
   return distanceFromHqInBlocks(blockNumber) * 264;
 }
+
+function distanceTravelledInFeet (start, destination) {
+  if (start < destination) {
+    return (destination - start) * 264;
+  } else {
+    return (start - destination)  * 264;
+  }
+}
