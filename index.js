30e0429f9d91782c1a599c785d47ca5f0110408b
ToAnh Tran <toanh.t.tran@gmail.com>
2018-10-19 17:13:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Code your solution in this file!
-function distanceTravelledInFeet(block1, block2) {
-  return ((block1 = block2) * 264)
-}
-
-function distanceFromHqInBlocks(num1, 1) {
-  return 43 - 1
+function distanceFromHqInBlocks(blockNum) {
+  if (blockNum > 42) {
+    return blockNum - 42;
+  } else {
+    return 42 blockNum;
+  }
 }
